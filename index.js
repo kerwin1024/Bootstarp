65221220b1c331576f2b3577ee9cc22538997499
kerwin1024 <kerwin_work@163.com>
2016-11-04 14:13:42 +0800

    Kerwin Bootstrap

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ var express = require('express');
 //实例化Express
 var app = express();
 
+//引入body-parser解析表单
+var bodyParser = require('body-parser');
+
+//path
+var path = require('path');
+
+//serve-favicon站点图标
+var favicon = require('serve-favicon');
+
 //handlebars 视图引擎及目录，视图文件扩展名
-var handlebars = require('express-handlebars');
-handlebars({
-    
+var handlebars = require('express-handlebars').create({
+
     //默认布局为main
     defaultLayout: 'main',
-    
+
     //设置模板引擎文件后缀为.hbs
     extname: '.hbs',
-    
+
     //创建一个Handlebars辅助函数，给出一个静态资源的链接
     helpers: {
         static: function (name) {
@@ -26,48 +34,43 @@ handlebars({
         }
     }
 });
-app.engine('hbs', handlebars({extname: ".hbs"}));
-
+//设置引擎
+app.engine('hbs', handlebars.engine);
+//设置视图
 app.set('view engine', 'hbs');
 
+//设置静态资源 static server
+app.use(express.static(__dirname + '/public'));
 
-//引入fs
-var fs = require('fs');
+//设置ico图标
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-//引入body-parser解析表单
-// app.use(require('body-parser')());
-var bodyParser = require('body-parser');    //老师的
+//引入fs（暂时用不上）
+// var fs = require('fs');
 
-//path
-var path = require('path');
-
-//serve-favicon站点图标
-var favicon = require('serve-favicon');
-app.use(favicon(path.join(__dirname, '/public', 'favicon.ico')));
-
-//导入自定义模块位置
+//导入自定义模块位置（路由、模型、模式）
 var routers_index = require('./routes/index');
 
-//端口
+//设置端口
 app.set('port', process.env.PORT || 3005);
 
-//静态资源
-// app.use(express.static(__dirname + '../public'));
 
-//路由
+//设置路由（根目录为上面定义的routers_index）
 app.use('/', routers_index);
 
-// 404
+//定制404页面
 app.use(function (req, res) {
     res.status(404);
-    res.render('errors/404');
+    //不使用布局
+    res.render('errors/404',{layout:null});
 });
 
-//500
+//定制500页面
 app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500);
-    res.render('errors/500');
+    //使用指定布局
+    res.render('errors/500', {layout: 'error', title: 'This pang with error layout'});
 });
 
 //listen
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /*GET home page*/
 router.get('/', function (req, res, next) {
-    res.render('layouts/main', {body: 'Express'});
+    //render(路由页面，上下文对象)
+    res.render('index', {title: 'Express'});
+});
+/*signup*/
+router.get('/signup',function(req,res,next){
+    res.render('signup',{title:'signup'});
+});
+
+/*contact us*/
+router.get('/contact',function (req,res,next) {
+    res.render('contact');
+});
+
+/*about page*/
+router.get('/about',function (req,res,next) {
+    res.renvder("about");
 });
 
 module.exports = router;
